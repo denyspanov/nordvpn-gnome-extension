e83777fca9dfc4c87c4f957b10f429a089f134bd
Denys Panov <denys.panov@gmail.com>
2022-04-17 15:06:04 +0300

    feature(Extension): Code cleanup

--- extension.js
+++ extension.js
@@ -8,6 +8,9 @@ const {St, GLib, Clutter} = imports.gi;
 
 let myPopup;
 
+const DEFAULT_SERVER = 'United_Kingdom';
+const COMMAND = 'nordvpn';
+
 const MyPopup = GObject.registerClass(
     class NordVpnExtension extends PanelMenu.Button {
 
@@ -15,29 +18,31 @@ const MyPopup = GObject.registerClass(
             icon.gicon = Gio.icon_new_for_string(Me.dir.get_path() + '/' + name + '.svg')
         }
 
-        _connect(icon, country) {
-            let params = ['nordvpn', 'connect', 'United_Kingdom']
-            if (country) {
-                params = ['nordvpn', 'connect', country]
-            }
+        _setProcessingStatus(icon) {
+            this._changeIcon(icon, 'processing');
+        }
 
+        _setConnectedStatus(icon) {
+            this._changeIcon(icon, 'connected');
+        }
+
+        _setDisconnectedStatus(icon) {
+            this._changeIcon(icon, 'disconnected');
+        }
+
+        _connect(icon, country) {
+            this._setProcessingStatus(icon)
+            let params = [COMMAND, 'connect', DEFAULT_SERVER]
+            if (country) {
+                params = [COMMAND, 'connect', country]
+            }
             let connect = Gio.Subprocess.new(
                 params,
                 Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
             );
             connect.communicate_utf8_async(null, null, (proc, res) => {
                 try {
-                    let [, stdout, stderr] = proc.communicate_utf8_finish(res);
-                    if (proc.get_successful()) {
-                        let rows = stdout.split('\n');
-                        let statusRow = rows[1];
-                        if (statusRow.search(' connected ') > 0) {
-                            this._changeIcon(icon, 'connected')
-                        }
-
-                    } else {
-                        throw new Error(stderr);
-                    }
+                    this.updateIconBasedOnStatus(icon)
                 } catch (e) {
                     logError(e);
                 }
@@ -45,23 +50,14 @@ const MyPopup = GObject.registerClass(
         }
 
         _disconnect(icon) {
+            this._setProcessingStatus(icon)
             let disconnect = Gio.Subprocess.new(
-                ['nordvpn', 'disconnect'],
+                [COMMAND, 'disconnect'],
                 Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
             );
             disconnect.communicate_utf8_async(null, null, (proc, res) => {
                 try {
-                    let [, stdout, stderr] = proc.communicate_utf8_finish(res);
-                    if (proc.get_successful()) {
-                        let rows = stdout.split('\n');
-                        let statusRow = rows[0];
-                        if (statusRow.search(' disconnected ') > 0) {
-                            this._changeIcon(icon, 'disconnected')
-                        }
-
-                    } else {
-                        throw new Error(stderr);
-                    }
+                    this.updateIconBasedOnStatus(icon)
                 } catch (e) {
                     logError(e);
                 }
@@ -74,31 +70,29 @@ const MyPopup = GObject.registerClass(
             this.add_child(icon);
             this.addConnectAction(icon);
             this.addDisconnectAction(icon);
-            this.defineStatus(icon);
+            this.updateIconBasedOnStatus(icon);
             this.addCountriesSubMenu(icon);
         }
 
         addCountriesSubMenu(icon) {
             let countriesList = Gio.Subprocess.new(
-                ['nordvpn', 'countries'],
+                [COMMAND, 'countries'],
                 Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
             );
             let subItem = new PopupMenu.PopupSubMenuMenuItem('Countries');
+            let popup = this;
             countriesList.communicate_utf8_async(null, null, (proc, res) => {
                 try {
                     let [, stdout, stderr] = proc.communicate_utf8_finish(res);
                     if (proc.get_successful()) {
                         let countries = stdout.toString().trim().split(', ');
-                        let i = 0
                         countries.forEach(function (name) {
-                            if (i > 0) {
-                                let item = new PopupMenu.PopupMenuItem(name);
-                                item.connect('activate', () => {
-                                    MyPopup._connect(icon, name)
-                                });
-                                subItem.menu.addMenuItem(item);
-                            }
-                            i++
+                            let clearedName = name.replace(/[^a-z0-9_]/gi, '');
+                            let item = new PopupMenu.PopupMenuItem(clearedName.replace(/_/gi, ' '));
+                            item.connect('activate', () => {
+                                popup._connect(icon, clearedName)
+                            });
+                            subItem.menu.addMenuItem(item);
                         })
                     } else {
                         throw new Error(stderr);
@@ -126,9 +120,10 @@ const MyPopup = GObject.registerClass(
             this.menu.addMenuItem(connect);
         }
 
-        defineStatus(icon) {
+        updateIconBasedOnStatus(icon) {
+            this._setProcessingStatus(icon)
             let defineStatus = Gio.Subprocess.new(
-                ['nordvpn', 'status'],
+                [COMMAND, 'status'],
                 Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
             );
             defineStatus.communicate_utf8_async(null, null, (proc, res) => {
@@ -138,11 +133,10 @@ const MyPopup = GObject.registerClass(
                         let rows = stdout.split('\n');
                         let statusRow = rows[0];
                         if (statusRow.search(' Connected') > 0) {
-                            icon.gicon = Gio.icon_new_for_string(Me.dir.get_path() + '/connected.svg')
+                            this._setConnectedStatus(icon)
                         } else {
-                            icon.gicon = Gio.icon_new_for_string(Me.dir.get_path() + '/disconnected.svg')
+                            this._setDisconnectedStatus(icon)
                         }
-
                     } else {
                         throw new Error(stderr);
                     }
